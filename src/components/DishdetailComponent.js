733313bf5ae26611d21220b72c9cec9e4324d52d
Daghan Altas <daghan@gmail.com>
2019-01-20 08:57:54 -0800

    Single Page Applications Part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,31 +1,12 @@
-import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, List } from 'reactstrap';
-import { DISHES } from '../shared/dishes';
+import React from 'react';
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
-class DishDetail extends Component {
-
-    constructor(props) {
-        super(props)
-
-        
-        this.state = {
-            dish : this.props.dish
-        }
-    }
-
-
-    componentDidMount() {
-        console.log("Mounted");
-    }
-
-    componentWillUnmount() {
-        console.log("will unmount");
-    }
-
-
-    renderDish(dish){
-        return(
+function RenderDish({dish}){
+    return (
+        <div className = 'col-12 col-md-5 m-1'>
             <Card>
                 <CardImg top src={dish.image} alt={dish.name} />
                 <CardBody>
@@ -33,53 +14,61 @@ class DishDetail extends Component {
                     <CardText>{dish.description}</CardText>
                 </CardBody>
             </Card>
+        </div>
+    );
+}
+
+
+function RenderComments({comments}){
+    if (comments === null ){
+        return(<div></div>);
+    }
+    else{
+        var commentsText = comments.map((comment) => {
+            return (
+                <ul key={comment.id} className="list-unstyled">
+                    <li>{comment.comment}</li>
+                    <li>-- {comment.author + " , " + 
+                            new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'})
+                            .format(new Date(Date.parse(comment.date)))} </li>
+                </ul>
+            );
+        });
+        return(
+            <div className = 'col-12 col-md-5 m-1'>
+                <h4>Comments</h4> 
+                {commentsText}
+            </div>
         );
     }
+}
 
-
-    renderComments(comments){
-        if (comments === null ){
-            return(<div></div>);
-        }
-        else{
-            var commentsText = comments.map((comment) => {
-                return (
-                    <ul key={comment.id} className="list-unstyled">
-                        <li>{comment.comment}</li>
-                        <li>-- {comment.author + " , " + 
-                                new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'})
-                                .format(new Date(Date.parse(comment.date)))} </li>
-                    </ul>
-                );
-            });
-            return(
-                <div>
-                    <h4>Comments</h4>
-                    {commentsText}
-                </div>
-            );
-        }
-    }
-
-    
-    render() {
-        if ((this.props.dish === null) || (this.props.dish === undefined)){
+const DishDetail = (props) => {
+        if ((props.dish === null) || (props.dish === undefined)){
             return(<div></div>);
         } else {
             return (
+                console.log("I am here"),
                 <div className="container">
                     <div className="row">
-                        <div  className="col-12 col-md-5 m-1">
-                           {this.renderDish(this.props.dish)} 
-                        </div>
-                        <div  className="col-12 col-md-5 m-1">
-                            {this.renderComments(this.props.dish.comments)}
-                        </div>
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12">
+                            <h3>{props.dish.name}</h3>
+                            <hr />
+                        </div>                
+                    </div>
+                    <div className="row">
+                        <RenderDish dish={props.dish} />
+                        <RenderComments comments = {props.comments} />
+                        
                     </div>
                 </div>
             );
         }
     }
-}
+
 
 export default DishDetail;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -22,11 +22,11 @@ class Main extends Component {
         leaders: LEADERS,
         promotions: PROMOTIONS
     };
-  }
+  };
 
+  
 
   render() {
-
     const HomePage = () => {
       return(
         <Home dish = {this.state.dishes.filter((dish) => dish.featured)[0]}
@@ -34,12 +34,21 @@ class Main extends Component {
               leader = {this.state.leaders.filter((leader) => leader.featured)[0]} />
       )
     }
+
+    const DishWithId = ({match}) => {
+      return(
+        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+                   comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
+
     return (
       <div>
         <Header />
         <Switch>
           <Route path='/home' component={HomePage} />
           <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} /> } />
+          <Route path='/menu/:dishId' component={DishWithId} />
           <Route exact path='/contactus' component={Contact} />
           <Redirect to='home' />
         </Switch>
